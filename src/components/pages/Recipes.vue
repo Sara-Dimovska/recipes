bb59ac0c23e07fea0b90b9a7b2c95e1696ea8ff6
t3mpV4r <sara.101903@student.ugd.edu.mk>
2019-11-21 21:34:22 +0100

    recipe list ingredients trim

--- src/components/pages/Recipes.vue
+++ src/components/pages/Recipes.vue
@@ -20,7 +20,7 @@
             </b-table-column>
 
             <b-table-column field="ingredients" label="Ingredients" sortable>
-              {{ recipes.row.ingredients }}
+              {{ showInstructions(recipes.row.ingredients,recipes.row.ingretients_number) }}
             </b-table-column>
 
             <b-table-column field="prep_instructions" label="Preparation instructions" sortable>
@@ -58,7 +58,7 @@ export default {
         "id": "1",
         "name":"Лиснато тесто со фил од тиква",
         "source": "https://moirecepti.mk/post/%D0%BB%D0%B8%D1%81%D0%BD%D0%B0%D1%82%D0%BE-%D1%82%D0%B5%D1%81%D1%82%D0%BE-%D1%81%D0%BE-%D1%84%D0%B8%D0%BB-%D0%BE%D0%B4-%D1%82%D0%B8%D0%BA%D0%B2%D0%B0",
-        "ingretients_number": 8,
+        "ingretients_number": 3,
         "ingredients": "500 гр. пире од тиква, 200 гр. крем сирење,250 гр. купено, готово лиснато тесто",
         "prep_instructions": "Во длабок сад се меша бла бла бла",
         "prep_time": "60:00"
@@ -68,7 +68,7 @@ export default {
         "name":"Лиснато тесто со фил од тиква",
         "source": "https://moirecepti.mk/post/%D0%BB%D0%B8%D1%81%D0%BD%D0%B0%D1%82%D0%BE-%D1%82%D0%B5%D1%81%D1%82%D0%BE-%D1%81%D0%BE-%D1%84%D0%B8%D0%BB-%D0%BE%D0%B4-%D1%82%D0%B8%D0%BA%D0%B2%D0%B0",
         "ingretients_number": 8,
-        "ingredients": "500 гр. пире од тиква, 200 гр. крем сирење,250 гр. купено, готово лиснато тесто",
+        "ingredients": "1, 2,3, готово лиснато тесто,250 гр. купено, готово лиснато тесто,250 гр. купено, готово лиснато тесто",
         "prep_instructions": "Во длабок сад се меша бла бла бла",
         "prep_time": "60:00"
     }
@@ -78,6 +78,12 @@ export default {
   methods: {
     navigateTo (route) {
       this.$router.push(route)
+    },
+    showInstructions(ingredients, num) {
+      if(num === 3)
+        return ingredients
+      var ingredients_split = ingredients.split(',').slice(0,3)
+      return ingredients_split.join() + ' (...)'
     }
   },
   async mounted () {
