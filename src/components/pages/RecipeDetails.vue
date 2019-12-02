d628df3d73ca40d5bd498d8d2afd9356013e2b0d
t3mpV4r <sara.101903@student.ugd.edu.mk>
2019-12-02 21:41:09 +0100

    recipe details

--- src/components/pages/RecipeDetails.vue
+++ src/components/pages/RecipeDetails.vue
@@ -1,30 +1,31 @@
 <template lang="pug">
-   v-layout(row text-xs-center style="padding:30px;")
-    v-flex(offset-xs3)
-      v-row
-        v-col(cols="8")
-          v-card 
-            v-card-title {{ recipe.name }}
-            v-card-title(style="font-size: medium;") Recipe source:
-            v-card-subtitle
-              a(v-bind:href="recipe.source") {{ recipe.source }}
-            v-card-title(style="font-size: medium;") Ingredients:
-            v-card-subtitle
-              li(v-for="ingredient in ingredients")
-                | {{ ingredient }}
-            v-card-title(style="font-size: medium;") Preparation time:
-            v-card-subtitle {{ prep_time_formated }}
-            v-card-title(style="font-size: medium;") Instructions:
-            v-card-subtitle {{ recipe.prep_instructions }}
-            v-btn(style="color:white; background-color:red; margin:10px;" @click.native="confirmDeleteDialog = true" ) Delete       
-      v-dialog(v-model="confirmDeleteDialog"  max-width="500")
-        v-card
-          v-card-title Are you sure you want to delete this recipe?
-          v-card-text By confirming this action this recipe will be delited.
-          v-card-actions
-            v-spacer
-            v-btn(color="error" @click="deleteRecipe()") Yes
-            v-btn(@click="confirmDeleteDialog = false") Cancel
+  v-layout(row text-xs-center style="padding:30px;")
+    v-row
+      v-col(cols="6")
+        v-card 
+          v-card-title {{ recipe.name }}
+          v-card-title(style="font-size: medium;") Recipe source:
+          v-card-subtitle
+            a(v-bind:href="recipe.source") {{ recipe.source }}
+          v-card-title(style="font-size: medium;") Ingredients:
+          v-card-subtitle
+            li(v-for="ingredient in ingredients")
+              | {{ ingredient }}
+          v-card-title(style="font-size: medium;") Preparation time:
+          v-card-subtitle {{ prep_time_formated }}
+          v-btn(style="color:white; background-color:red; margin:10px;" @click.native="confirmDeleteDialog = true" ) Delete 
+      v-col(cols="6")
+        v-card 
+          v-card-title(style="font-size: medium;") Instructions:
+          v-card-subtitle {{ recipe.prep_instructions }}    
+    v-dialog(v-model="confirmDeleteDialog"  max-width="500")
+      v-card
+        v-card-title Are you sure you want to delete this recipe?
+        v-card-text By confirming this action this recipe will be delited.
+        v-card-actions
+          v-spacer
+          v-btn(color="error" @click="deleteRecipe()") Yes
+          v-btn(@click="confirmDeleteDialog = false") Cancel
 </template>
 
 <script>
